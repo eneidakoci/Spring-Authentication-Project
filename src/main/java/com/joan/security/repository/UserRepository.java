f10cdb0d3cb00ffd7e3f9cffa22a137c94933d8c
Eneida Koçi <99421177+eneidakoci@users.noreply.github.com>
2023-11-03 18:58:50 +0100

    Update UserRepository.java

--- src/main/java/com/joan/security/repository/UserRepository.java
+++ src/main/java/com/joan/security/repository/UserRepository.java
@@ -8,5 +8,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User save(User user);
 
-    User findUserByLogin(String login);
+    User findUserDTOByLogin(String login);
 }
