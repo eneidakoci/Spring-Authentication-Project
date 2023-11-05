5c068e620226b3ebf997534057ffbf1db25efefd
Eneida Koçi <99421177+eneidakoci@users.noreply.github.com>
2023-11-05 15:29:18 +0100

    Update AuthenticationController.java

--- src/main/java/com/joan/security/controller/AuthenticationController.java
+++ src/main/java/com/joan/security/controller/AuthenticationController.java
@@ -3,8 +3,12 @@ package com.joan.security.controller;
 import com.joan.security.config.UserAuthenticationProvider;
 import com.joan.security.dto.SignUpDTO;
 import com.joan.security.dto.User;
+import com.joan.security.dto.UserDTO;
+import com.joan.security.mapper.UserMapper;
 import com.joan.security.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,21 +25,41 @@ import java.text.ParseException;
 public class AuthenticationController {
     private final UserService userService;
     private final UserAuthenticationProvider userAuthenticationProvider;
+    @Autowired
+    private UserMapper userMapper;
 
-    @PostMapping("/log-in")
-    public ResponseEntity<User> login(@AuthenticationPrincipal User user) throws ParseException {
-        user.setToken(userAuthenticationProvider.createToken(user));
-        return ResponseEntity.ok(user);
-    }
+    //    @PostMapping("/log-in")
+//    public ResponseEntity<User> login(@AuthenticationPrincipal User userDTO) throws ParseException {
+//        userDTO.setToken(userAuthenticationProvider.createToken(userDTO));
+//        return ResponseEntity.ok(userDTO);
+//    }
+    /*working*/
+@PostMapping("/log-in")
+public ResponseEntity<UserDTO> login(@AuthenticationPrincipal User user) throws ParseException {
+    UserDTO userDTO = userMapper.toDTO(user);
+    userDTO.setToken(userAuthenticationProvider.createToken(user));
+    return ResponseEntity.ok(userDTO);
+}
+//    @PostMapping("/log-in")
+//    public ResponseEntity<UserDTO> login(@AuthenticationPrincipal UserDTO userDTO) throws ParseException {
+//        if (userDTO != null) {
+//            User user = userMapper.toEntity(userDTO);
+//            user.setToken(userAuthenticationProvider.createToken(user));
+//            userDTO = userMapper.toDTO(user);
+//            return ResponseEntity.ok(userDTO);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+//        }
+//    }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<User> register(@RequestBody SignUpDTO signUpDTO) {
-        User createdUser = userService.signUp(signUpDTO);
+    public ResponseEntity<UserDTO> register(@RequestBody SignUpDTO signUpDTO) {
+        UserDTO createdUser = userMapper.toDTO(userService.signUp(signUpDTO));
         return ResponseEntity.ok(createdUser);
     }
 
     @PostMapping("/log-out")
-    public ResponseEntity<Void> logout(@AuthenticationPrincipal User signUpDTO) {
+    public ResponseEntity<Void> logout(@AuthenticationPrincipal UserDTO signUpDTO) {
         SecurityContextHolder.clearContext();
         return ResponseEntity.noContent().build();
     }
